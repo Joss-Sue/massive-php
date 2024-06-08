cbaa10eb114bcb84c0fa4f94b014dbdf1fc2191d
Joss-Sue <josue_croow@hotmail.com>
2024-06-07 22:40:24 -0600

    precio del carrito fix

--- api/compraPedidoController.php
+++ api/compraPedidoController.php
@@ -108,7 +108,7 @@ function obtenerPedidos($idUsuario, $conexion){
         
            
             foreach ($pedidos as $indice => $pedido) {
-                $sqlQuery="SELECT ventas.articulosTotales, productos.nombreProd, productos.descripcionProd, productos.idProd, productos.precioProd
+                $sqlQuery="SELECT ventas.articulosTotales, productos.nombreProd, productos.descripcionProd, productos.idProd, ventas.precio
                 FROM ventas
                 JOIN productos ON ventas.idProductoVenta = productos.idProd
                 WHERE ventas.idPedido = :idPedido";
